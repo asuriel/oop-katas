25a15d878f2e2cfee1e7752be713b4d7c1def1fc
asurielm <a.suriel@yahoo.co.uk>
2012-06-19 16:05:53 +0100

    ASM: myCircle renamed and ignored

---
+++ geometrical-shapes-tdd/src/tddshapes/MyCircle.java
@@ -0,0 +1,21 @@
+package tddshapes;
+
+public class MyCircle implements Shape{
+    private double radius;
+    private double diameter;
+
+    public MyCircle(double radius) {
+        this.radius = radius;
+        diameter = radius*2;
+    }
+
+    public double calculateArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public Rectangle boundingRectangle() {
+        return new Rectangle(diameter,diameter);
+    }
+
+}
